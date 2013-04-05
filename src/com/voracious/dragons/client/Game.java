3074717b40e783d11e7169ab1b2ec682035332ba
Andrew Kallmeyer <kallmeyeras@gmail.com>
2013-04-05 19:57:55 -0400

    Added logging system Also added thread system to Game

--- src/com/voracious/dragons/client/Game.java
+++ src/com/voracious/dragons/client/Game.java
@@ -1,21 +1,67 @@
-package com.voracious.dragons;
+package com.voracious.dragons.client;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 
-public class Game extends Canvas {
+import org.apache.log4j.Logger;
+
+public class Game extends Canvas implements Runnable {
     private static final long serialVersionUID = 1L;
-    
+
     public static final String NAME = "Here There Be Dragons";
+    public static final int WIDTH = 720;
+    public static final int HEIGHT = 480;
+    public static final int FPS = 60;
+    private static final boolean printFPS = true;
+
+    private static Logger logger = Logger.getLogger(Game.class);
+    private static Thread thread;
+    private static boolean running = false;
+    
+
+    public Game() {
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
+
+    public void render() {
+    }
+
+    public void tick() {
+    }
+
+    @Override
+    public void run() {
+        long unprocessedTicks = 0L;
+        long ticksPerNs = 1000000000L/FPS;
+        long lastTime = System.nanoTime();
+        
+        long lastFpsTime = System.currentTimeMillis();
+        int frameCount = 0;
+        int tickCount = 0;
+        
+        while(running){
+            if(printFPS && System.currentTimeMillis() - lastFpsTime > 1000){
+                System.out.println(frameCount + " fps; " + tickCount + " tps");
+                frameCount = 0;
+                tickCount = 0;
+                lastFpsTime = System.currentTimeMillis();
+            }
+        }
+    }
 
     public void init() {
-        System.out.println("init");
+        logger.debug("init");
+        thread = new Thread(this);
     }
 
     public void start() {
-        System.out.println("start");
+        logger.debug("start");
+        running = true;
+        thread.start();
     }
 
     public void stop() {
-        System.out.println("stop");
+        logger.debug("stop");
+        running = false;
     }
 }
--- src/com/voracious/dragons/client/Main.java
+++ src/com/voracious/dragons/client/Main.java
@@ -1,16 +1,36 @@
-package com.voracious.dragons;
+package com.voracious.dragons.client;
 
 import java.applet.Applet;
 import java.awt.Frame;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.IOException;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 
 public class Main extends Applet implements WindowListener {
     private static final long serialVersionUID = 1L;
+
+    public static final String logfile = "output.log";
+
+    private static Logger logger;
     private static Game game;
 
     public static void main(String[] args) {
         Frame window = new Frame(Game.NAME);
+
+        initLog4J();
+        logger.setLevel(Level.INFO);
+        if (args.length > 0) {
+            if (args[0] == "--debug") {
+                logger.setLevel(Level.ALL);
+            }
+        }
+
         game = new Game();
         game.init();
         window.add(game);
@@ -18,32 +38,46 @@ public class Main extends Applet implements WindowListener {
         window.addWindowListener(new Main());
         window.setLocationRelativeTo(null);
         window.pack();
+        window.setResizable(false);
         window.setVisible(true);
 
         game.start();
     }
-    
+
     @Override
     public void windowClosing(WindowEvent arg0) {
         game.stop();
         System.exit(0);
     }
-    
+
     @Override
-    public void init(){
+    public void init() {
+        initLog4J();
+        logger.setLevel(Level.ALL);
         game = new Game();
         this.add(game);
         game.init();
     }
-    
+
     @Override
-    public void start(){
+    public void start() {
         game.start();
     }
-    
+
     @Override
-    public void stop(){
+    public void stop() {
         game.stop();
+    }
+
+    public static void initLog4J() {
+        logger = Logger.getLogger(Main.class);
+
+        Logger.getRootLogger().addAppender(new ConsoleAppender(new PatternLayout("%-4r [%t] %-5p %c %x - %m%n")));
+        try {
+            Logger.getRootLogger().addAppender(new FileAppender(new PatternLayout("%-4r [%t] %-5p %c %x - %m%n"), logfile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
