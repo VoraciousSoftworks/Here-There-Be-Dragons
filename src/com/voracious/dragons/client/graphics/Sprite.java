3a38541ede605cbfec06341ae15383857886eff8
Andrew Kallmeyer <kallmeyeras@gmail.com>
2013-04-05 21:11:50 -0400

    Added Sprite class

---
+++ src/com/voracious/dragons/client/graphics/Sprite.java
@@ -0,0 +1,38 @@
+package com.voracious.dragons.client.graphics;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.log4j.Logger;
+
+public class Sprite {
+
+    BufferedImage image;
+    
+    public Sprite(String filepath){
+        try {
+            image = ImageIO.read(Sprite.class.getResourceAsStream(filepath));
+        } catch (IOException e) {
+            Logger.getLogger(Sprite.class).error("Could not read image", e);
+        }
+    }
+    
+    public void draw(Graphics2D g, int x, int y) {
+        g.drawImage(image, x, y, null);
+    }
+    
+    public int getWidth(){
+        return image.getWidth();
+    }
+    
+    public int getHeight(){
+        return image.getHeight();
+    }
+    
+    public void setImage(BufferedImage image){
+        this.image = image;
+    }
+}
