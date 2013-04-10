013225f9b8eae6b211d53f70a05186b7c1d489ed
Aaron <bakeraj4@miamioh.edu>
2013-04-09 22:07:36 -0400

    Added the Path and Dragon Classes. Added a method to Unit.
    The Path class has an array list of Vec2D.Short's. It will be used to
    create the points that the units will move through. The Dragon class is
    a unit and will be a dragon in the game. The method added to Unit is
    setWayPoint. This will be the point that the unit will start to move to.

---
+++ src/com/voracious/dragons/client/units/Dragon.java
@@ -0,0 +1,16 @@
+package com.voracious.dragons.client.units;
+
+public class Dragon extends Unit {
+
+	public Dragon(String filename, int[] numFrames, int width, int height) {
+		super(filename, numFrames, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public byte getUnitId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/com/voracious/dragons/client/units/Unit.java
+++ src/com/voracious/dragons/client/units/Unit.java
@@ -3,6 +3,7 @@ package com.voracious.dragons.client.units;
 import com.voracious.dragons.client.graphics.Entity;
 
 public abstract class Unit extends Entity {
+	
     public Unit(String filename, int[] numFrames, int width, int height) {
         super(filename, numFrames, width, height);
     }
@@ -11,4 +12,8 @@ public abstract class Unit extends Entity {
      * @return the id of this unit type, it should be unique
      */
     public abstract byte getUnitId();
+    
+    public void setWayPoint(Short wayP){
+    	
+    }
 }
