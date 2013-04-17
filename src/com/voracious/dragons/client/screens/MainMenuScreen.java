6e62ede18e750c53ed5534c2ab41b47486dc78ce
Andrew Kallmeyer <kallmeyeras@gmail.com>
2013-04-17 16:00:48 -0400

    Removed some methods from MainMenuScreen

--- src/com/voracious/dragons/client/screens/MainMenuScreen.java
+++ src/com/voracious/dragons/client/screens/MainMenuScreen.java
@@ -2,9 +2,6 @@ package com.voracious.dragons.client.screens;
 
 import java.awt.Graphics2D;
 
-import org.apache.log4j.Logger;
-
-import com.voracious.dragons.client.Game;
 import com.voracious.dragons.client.graphics.Screen;
 import com.voracious.dragons.client.graphics.Sprite;
 import com.voracious.dragons.client.graphics.ui.Button;
@@ -13,7 +10,6 @@ import com.voracious.dragons.client.utils.InputHandler;
 public class MainMenuScreen extends Screen {
 	public static final int WIDTH = 2160/3;
     public static final int HEIGHT = 1440/3;
-    private static Logger logger = Logger.getLogger(Game.class);
     private Button playTurn,playNew,spectate,stats;
     private Sprite background;
     
@@ -26,13 +22,13 @@ public class MainMenuScreen extends Screen {
 		this.spectate=new Button("Spectate a Game",250,220);
 		this.stats=new Button("See Stats",425,220);
 		
-		this.setBackground(new Sprite("/mainMenuBackground.png"));
+		background = new Sprite("/mainMenuBackground.png");
 		InputHandler.registerScreen(this);
 	}
 
 	@Override
 	public void render(Graphics2D g) {
-		this.getBackground().draw(g, 0, 0);
+		background.draw(g, 0, 0);
 		this.playTurn.draw(g);
 		this.playNew.draw(g);
 		this.spectate.draw(g);
@@ -42,36 +38,5 @@ public class MainMenuScreen extends Screen {
 
 	@Override
 	public void tick() {
-		// TODO Auto-generated method stub
-
 	}
-
-	/**
-	 * @return the logger
-	 */
-	public static Logger getLogger() {
-		return logger;
-	}
-
-	/**
-	 * @param logger the logger to set
-	 */
-	public static void setLogger(Logger logger) {
-		MainMenuScreen.logger = logger;
-	}
-
-	/**
-	 * @return the background
-	 */
-	public Sprite getBackground() {
-		return background;
-	}
-
-	/**
-	 * @param background the background to set
-	 */
-	public void setBackground(Sprite background) {
-		this.background = background;
-	}
-
 }
