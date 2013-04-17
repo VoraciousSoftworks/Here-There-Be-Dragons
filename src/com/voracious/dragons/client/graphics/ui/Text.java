6fae4afd7e207fbd30436b5c034b8104e8809a24
Brandon Langmeier <blangmeier@hotmail.com>
2013-04-17 10:57:49 -0400

    Set up a menu box that tells you what mode you are in. Currently crashed pending Andy's fix. ~Brandon

--- src/com/voracious/dragons/client/graphics/ui/Text.java
+++ src/com/voracious/dragons/client/graphics/ui/Text.java
@@ -1,5 +1,6 @@
 package com.voracious.dragons.client.graphics.ui;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
@@ -8,20 +9,31 @@ import com.voracious.dragons.client.graphics.Drawable;
 public class Text implements Drawable {
 
 	private int x, y;
+	private Color color;
 	private String text;
 	private BufferedImage image;
 	private boolean needsRefresh; 
 	
-	public Text(String text, int x, int y){
+	public Text(String text, int x, int y, Color c){
 		this.x = x;
 		this.y = y;
+		this.color = c;
 		this.text = text;
 		needsRefresh = true;
+	}
+	
+	public Text(){
+		this.x = 0;
+		this.y = 0;
+		this.color = Color.BLACK;
+		this.text = "No text set";
+		needsRefresh=true;
 	}
 	
 	@Override
 	public void draw(Graphics2D g) {
 		if(needsRefresh){
+			g.setColor(color);
 			int height = g.getFontMetrics().getHeight();
 			int width = g.getFontMetrics().stringWidth(text);
 			
@@ -44,5 +56,13 @@ public class Text implements Drawable {
 	public void setLocation(int x, int y){
 		this.x = x;
 		this.y = y;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
 	}
 }
