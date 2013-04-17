74e9417c5b8af156f30372f9d03160a3e72ab3db
Andrew Kallmeyer <kallmeyeras@gmail.com>
2013-04-17 12:07:19 -0400

    Added button class

--- src/com/voracious/dragons/client/graphics/ui/Button.java
+++ src/com/voracious/dragons/client/graphics/ui/Button.java
@@ -2,12 +2,20 @@ package com.voracious.dragons.client.graphics.ui;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.voracious.dragons.client.graphics.Drawable;
 
 public class Button implements Drawable {
 
+	public static final int ACTION_BUTTON_PRESSED = 1;
+	
 	public static final Color defaultBackground = new Color(0xDDDDDD);
 	public static final Color defaultBoarder = new Color(0xEEEEEE);
 	public static final int defaultPadding = 5;
@@ -17,7 +25,11 @@ public class Button implements Drawable {
 	private Text text;
 	private BufferedImage image;
 	
+	private List<ActionListener> listeners;
+	
 	public Button(String text) {
+		listeners = new LinkedList<ActionListener>();
+		
 		this.text = new Text(text, defaultPadding, defaultPadding);
 
 		width = this.text.getWidth() + defaultPadding*2;
@@ -32,7 +44,23 @@ public class Button implements Drawable {
 		ig.setColor(defaultBoarder);
 		ig.drawRect(0, 0, width, height);
 		
+		this.x = 4;
+		this.y = 4;
+		
 		this.text.draw(ig);
+	}
+	
+	public void addActionListener(ActionListener l){
+		listeners.add(l);
+	}
+	
+	public void mouseClicked(int x, int y){
+		if(x > this.x && x < this.width && y > this.y && y < this.height){
+			Iterator<ActionListener> it = listeners.iterator();
+			while(it.hasNext()){
+				it.next().actionPerformed(new ActionEvent(this, ACTION_BUTTON_PRESSED, "pressed"));
+			}
+		}
 	}
 	
 	@Override
