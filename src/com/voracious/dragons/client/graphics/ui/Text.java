a2bd81c80f4ea5a9b6a81d22503e87330dfd76c6
Andrew Kallmeyer <kallmeyeras@gmail.com>
2013-04-17 11:54:03 -0400

    Fixed Text height and changed the Text defualt constructor

--- src/com/voracious/dragons/client/graphics/ui/Text.java
+++ src/com/voracious/dragons/client/graphics/ui/Text.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 import com.voracious.dragons.client.graphics.Drawable;
@@ -21,25 +22,28 @@ public class Text implements Drawable {
 		this.y = y;
 		this.text = text;
 		needsRefresh = true;
+		color = Color.black;
 		refresh(null);
 	}
 	
-	public Text(){
-		this.x = 0; this.y = 0;
-		this.text = "No text set";
+	public Text(String text){
+		this.x = 0; 
+		this.y = 0;
+		this.text = text;
 		needsRefresh = true;
+		color = Color.black;
 		refresh(null);
 	}
 	
 	private void refresh(Font f){
 		image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-		FontMetrics metrics;
+		
 		if(f == null){
-			metrics = image.getGraphics().getFontMetrics();
-		}else{
-			metrics = image.getGraphics().getFontMetrics(f);
+			f = image.getGraphics().getFont();
 		}
 		
+		FontMetrics metrics = image.getGraphics().getFontMetrics(f);
+
 		int height = metrics.getHeight();
 		int width = metrics.stringWidth(text);
 
@@ -52,7 +56,7 @@ public class Text implements Drawable {
 		
 		ig.setColor(color);
 		
-		ig.drawString(text, 0, height);
+		ig.drawString(text, 0, metrics.getAscent());
 
 		needsRefresh = false;
 	}
