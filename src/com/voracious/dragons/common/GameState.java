c168a294c9e540d190a2dc057487d79e1546bf4d
Andrew Kallmeyer <kallmeyeras@gmail.com>
2013-04-22 20:30:10 -0400

    Fixed formatting in GameState

--- src/com/voracious/dragons/common/GameState.java
+++ src/com/voracious/dragons/common/GameState.java
@@ -6,45 +6,46 @@ import com.voracious.dragons.client.graphics.Drawable;
 import com.voracious.dragons.client.towers.Tower;
 import com.voracious.dragons.client.units.Unit;
 
-public class GameState implements Drawable{
-	
-	private List<Tower> towers;
-	private List<Unit> units;
-	
-	public GameState(){
-		super();
-	}
+public class GameState implements Drawable {
 
-	@Override
-	public void draw(Graphics2D g) {
-		 for(Tower t : towers)
-			 t.draw(g);
-		 for(Unit u : units)
-			 u.draw(g);
-	}
-	
-	public void tick(){
-		for(Tower t : towers)
-			t.tick();
-		for(Unit u : units)
-			u.tick();
-	}
-	
-	public void addTower(Tower t){
-		towers.add(t);
-	}
-	
-	public void removeTower(Tower t){
-		towers.remove(t);
-	}
-	
-	public void addUnit(Unit u){
-		units.add(u);
-	}
-	
-	public void removeUnit(Unit u){
-		units.remove(u);
-	}
-	
-	
+    private List<Tower> towers;
+    private List<Unit> units;
+
+    public GameState() {
+        super();
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        for (Tower t : towers)
+            t.draw(g);
+        
+        for (Unit u : units)
+            u.draw(g);
+    }
+
+    public void tick() {
+        for (Tower t : towers)
+            t.tick();
+        
+        for (Unit u : units)
+            u.tick();
+    }
+
+    public void addTower(Tower t) {
+        towers.add(t);
+    }
+
+    public void removeTower(Tower t) {
+        towers.remove(t);
+    }
+
+    public void addUnit(Unit u) {
+        units.add(u);
+    }
+
+    public void removeUnit(Unit u) {
+        units.remove(u);
+    }
+
 }
