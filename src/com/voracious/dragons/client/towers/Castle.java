b18bb2f88b25f1cb15d7c30de1e8f7e06551a175
Aaron <bakeraj4@miamioh.edu>
2013-04-22 22:26:53 -0400

    Added the destruction of the castle images.

--- src/com/voracious/dragons/client/towers/Castle.java
+++ src/com/voracious/dragons/client/towers/Castle.java
@@ -14,9 +14,38 @@ public class Castle extends Entity {
 	public static int[] numFrames = {1};
 	public static int width = 300;
 	public static int height = 300;
+	private double chp=100;
+	private double fullhp=100;
+	private double def=5;
 	
 	public Castle() {
 		super(filename, numFrames, width, height);
 	}
-
+	
+	public Castle(String file, int HP) {
+		super(file, numFrames, width, height);
+		chp=HP;
+	}
+	
+	public void takeDamage(double attk){
+		double damage=def-attk;
+		chp-=damage;
+		double percent=chp/fullhp;
+		if(percent<=1.0&&percent>0.7){
+			//nothing, stays at full heatlh
+		}
+		else if(percent<=0.7&&percent>0.4){
+			//change to a semi damaged castle
+		}
+		else if(percent<=0.4&&percent>0.1){
+			//almost gone
+		}
+		else{
+			//rubble
+		}
+	}
+	
+	public double getCHP(){
+		return this.chp;
+	}
 }
